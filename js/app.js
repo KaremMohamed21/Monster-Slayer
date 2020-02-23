96f186c7350bd28e881453d15b9d82e56aabc592
Karem Mohamed <karemkero21@gmail.com>
2020-02-23 23:11:48 +0200

    Finish the game logic

--- js/app.js
+++ js/app.js
@@ -4,45 +4,79 @@ new Vue({
 		playerHealth: 100,
 		monsterHealth: 100,
 		gameIsRunning: false,
+		turns: []
 	},
 	methods: {
 		startGame: function() {
 			this.gameIsRunning = true;
 			this.playerHealth = 100;
 			this.monsterHealth = 100;
+			this.turns = [];
 		},
 		attack: function() {
-			var max = 10;
-			var min = 3;
-			var damage = Math.max(Math.floor((Math.random() * max) + 1), min);
-			this.monsterHealth -= damage;
 
-			if (this.monsterHealth <= 0) {
-				alert('You Won!');
-				this.gameIsRunning = false;
-				return;
-			}
+			this.damageAttack(10, 3);
 
-			var max = 12;
-			var min = 5;
-			var damage = Math.max(Math.floor((Math.random() * max) + 1), min);
-			this.playerHealth -= damage;
-
-			if (this.playerHealth <= 0) {
-				alert('You Lost!');
-				this.gameIsRunning = false;
-			}
-
+			this.monsterAttack();
 
 		},
 		specialAttack: function() {
 
+			this.damageAttack(10, 3);
+
+			this.monsterAttack();
+
 		},
 		heal: function() {
+			this.playerHealth += 10;
+
+			this.turns.unshift({
+				isPlaying: true,
+				text: "You Heal for 10" 
+			});
+
+			this.monsterAttack();
 
 		},
 		giveUp: function() {
+			this.newGame();
 
+		},
+		damageAttack: function(max, min) {
+			var damage = Math.max(Math.floor((Math.random() * max) + 1), min);
+			this.monsterHealth -= damage;
+
+			this.turns.unshift({
+				isPlaying: true,
+				text: "You Hits Monster for " + damage
+			});
+
+			if (this.monsterHealth <= 0) {
+				alert('You Won!');
+				this.newGame();
+				return;
+			}
+
+		},
+		monsterAttack: function() {
+			var damage = Math.max(Math.floor((Math.random() * 12) + 1), 5);
+			this.playerHealth -= damage;
+
+			this.turns.unshift({
+				isPlaying: true,
+				text: "Monster Hits You for " + damage
+			});
+
+			if (this.playerHealth <= 0) {
+				alert('You Lost!');
+				this.newGame();			
+			}
+		},
+		newGame: function() {
+			this.gameIsRunning = false;
+			this.playerHealth = 100;
+			this.monsterHealth = 100;
+			this.turns = [];
 		}
 	}
 
